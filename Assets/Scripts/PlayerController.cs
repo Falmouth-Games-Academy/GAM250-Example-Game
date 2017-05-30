14e979950f7e9834403947e6fc221c33d075d261
McDonald <Brian.McDonald@falmouth.ac.uk>
2017-05-30 16:57:41 +0100

    Player Movement Implemented

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 moveAxis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 moveDelta = moveAxis * movementSpeed * Time.deltaTime;
+        transform.position += new Vector3(moveDelta.x, moveDelta.y, 0.0f);
+	}
+}
