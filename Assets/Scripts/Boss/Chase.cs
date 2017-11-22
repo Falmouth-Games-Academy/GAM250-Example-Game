9514379647ade217c9d07541d5942077a620e1ec
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2017-11-22 13:44:29 +0000

    Added in Chase State

---
+++ Assets/Scripts/Boss/Chase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+
+public class Chase : StateBehaviour
+{
+    public GameObject player;
+    public GameObject boss;
+
+    public CharacterController characterController;
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 direction = player.transform.position - boss.transform.position;
+        direction.Normalize();
+        characterController.Move(direction * 2.0f * Time.deltaTime);
+    }
+}
