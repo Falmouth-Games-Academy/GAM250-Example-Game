0d174b4827c49403cec712e425a89b9defc8b52c
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2017-11-22 14:38:05 +0000

    Started to break down behaviours

---
+++ assets/Scripts/Boss/RotateTowardsTarget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+
+public class RotateTowardsTarget : ActionNode
+{
+    public override Status Update()
+    {
+        return Status.Running;
+    }
+}
